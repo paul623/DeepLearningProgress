1de42b8b9ed2348dd74dbc22e801880de6eaa29c
巴塞罗那的余晖 <zhubaoluo@outlook.com>
2023-09-20 21:33:49 +0800

    9.20

---
+++ 23_nn_loss.py
@@ -0,0 +1,14 @@
+import torch
+from torch.nn import MSELoss
+
+inputs = torch.tensor([1, 2, 3], dtype=torch.float32)
+targets = torch.tensor([1, 2, 5], dtype=torch.float32)
+
+inputs = torch.reshape(inputs, (1, 1, 1, 3))
+targets = torch.reshape(targets, (1, 1, 1, 3))
+
+loss = MSELoss()
+
+result = loss(inputs, targets)
+
+print(result)
---
+++ 24_nn_optim.py
@@ -0,0 +1,47 @@
+import torch
+import torchvision.datasets
+from torch import nn
+from torch.nn import Conv2d, MaxPool2d, Flatten, Linear, Sequential
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+dataset = torchvision.datasets.CIFAR10("../dataset", train=False, transform=torchvision.transforms.ToTensor(),
+                                       download=True)
+dataLoader = DataLoader(dataset=dataset, batch_size=64)
+
+
+class MyModel(nn.Module):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.model1 = Sequential(
+            Conv2d(in_channels=3, out_channels=32, kernel_size=5, padding=2),
+            MaxPool2d(2),
+            Conv2d(in_channels=32, out_channels=32, kernel_size=5, padding=2),
+            MaxPool2d(2),
+            Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2),
+            MaxPool2d(2),
+            Flatten(),
+            Linear(in_features=64 * 4 * 4, out_features=64),
+            Linear(in_features=64, out_features=10)
+        )
+
+    def forward(self, x):
+        x = self.model1(x)
+        return x
+
+
+loss = nn.CrossEntropyLoss()
+myModel = MyModel()
+optim = torch.optim.SGD(myModel.parameters(), lr=0.01)
+for epoch in range(20):
+    running_loss = 0.0
+    for data in dataLoader:
+        imgs, targets = data
+        outputs = myModel(imgs)
+        result_loss = loss(outputs, targets)
+        optim.zero_grad() # 清除上一轮的梯度
+        result_loss.backward() # 计算梯度
+        optim.step() # 利用梯度进行优化
+        running_loss = running_loss + result_loss
+    print(running_loss)
+
